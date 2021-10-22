3c2dda501a76c7fae47970c129a54e2ee0e02446
mrLuisFer <mrluisfeer@gmail.com>
2021-10-22 09:46:41 -0500

    [fix] move formatDate to a utils dir

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,21 +1,5 @@
 import {dateTextElement} from './htmlElements'
-
-console.log(dateTextElement)
-
-const getFormatDate = (): string => {
-	let hours: number = new Date().getHours()
-	let minutes: number = new Date().getMinutes()
-	hours = hours % 12;
-	hours = hours ? hours : 12; // the hour '0' should be '12'
-	minutes = minutes < 10 ? 0 + minutes : minutes;
-	const ampm: string = hours >= 12 ? 'pm' : 'am';
-	console.log(`${hours}:${minutes}`)
-	console.log(ampm)
-	console.log(minutes)
-	console.log(hours)
-
-	return `${hours}:${minutes} ${ampm}`
-}
+import {getFormatDate} from './utils'
 
 window.addEventListener('load', () => {
 	console.log('page loaded')
