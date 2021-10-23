bb90431dbb2ff594b56e847c4f9ad01b75c57544
mrLuisFer <mrluisfeer@gmail.com>
2021-10-23 14:23:20 -0500

    [feat] add lit library and create an example component

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
 import './scss/index.scss'
 import './ts'
 
-const app: HTMLDivElement | HTMLElement = document.querySelector<HTMLDivElement>('#app')!
+import { LitElement, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+
+const app: HTMLDivElement | HTMLElement =
+  document.querySelector<HTMLDivElement>('#app')!
 console.log(app)
+
+@customElement('my-app')
+export class MyApp extends LitElement {
+  @property()
+  name: string = 'Luis'
+
+  render() {
+    return html`
+      <div>
+        <h1>Un lit element</h1>
+      </div>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'my-app': MyApp
+  }
+}
