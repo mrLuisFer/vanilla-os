1928ad4962d8b9b48f17629bd46de26a5a8f35d1
mrLuisFer <mrluisfeer@gmail.com>
2021-12-08 21:01:52 -0600

    add sass and svelte-preprocess

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
+import * as sass from 'sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte({
+    preprocess: sveltePreprocess({
+      sass: {
+        async: true,
+        implementation: sass,
+      }
+    }),
+    css: (css) => {
+      css.write('public/bundle.css')
+    },
+  })],
 })
