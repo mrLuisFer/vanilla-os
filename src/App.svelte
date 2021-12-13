7d2857b23857c6d9a676dab3b7fe1b4b027c6537
mrLuisFer <mrluisfeer@gmail.com>
2021-12-13 12:34:09 -0600

    add nord colorscheme and add some styles

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,6 @@
 
   let name: string = "";
   let password: string = "";
-
 </script>
 
 <main>
@@ -15,13 +14,47 @@
   {:else}
     <Login bind:name={name} bind:password={password} />
   {/if}
-  <p>{name}</p>
-  <p>{password}</p>
 </main>
 
-<style>
+<style lang="scss" global>
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 16px;
+    // Nord Palette
+    --polar-night-0: #2e3440;
+    --polar-night-1: #3b4252;
+    --polar-night-2: #434c5e;
+    --polar-night-3: #4C566A;
+
+    --snow-storm-0: #D8DEE9;
+    --snow-storm-1: #E5E9F0;
+    --snow-storm-2: #ECEFF4;
+
+    --frost-0: #8FBCBB;
+    --frost-1: #88C0D0;
+    --frost-2: #81A1C1;
+    --frost-3: #5E81AC;
+
+    --aurora-red: #BF616A;
+    --aurora-orange: #D08770;
+    --aurora-yellow: #EBCB8B;
+    --aurora-green: #A3BE8C;
+    --aurora-purple: #B48EAD;
+  }
+
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
   }
 </style>
--- src/components/common/Header.svelte
+++ src/components/common/Header.svelte
@@ -1,10 +1,26 @@
-<script>
+<script lang="ts">
 </script>
 
 <header class="header">
-  <h1 class="header-title">title</h1>
+  <div class="header_tiled-window">
+    <p>Tiled, Float</p>
+  </div>
 </header>
 
 <style lang="scss">
-  
+  .header {
+    display: flex;
+    background: var(--polar-night-0);
+    padding: 0.5rem 1.5rem;
+    
+    &_tiled-window {
+      background: var(--aurora-yellow);
+      padding: 0.5rem 1rem;
+      font-family: 'Roboto Mono', sans-serif;
+
+      p {
+        margin: 0;
+      }
+    }
+  }
 </style>
