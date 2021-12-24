93bb79eb8f11f112158c6624c7208d58f6b4ac0b
mrLuisFer <mrluisfeer@gmail.com>
2021-12-23 23:40:42 -0600

    fix config for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,36 @@
+// @typescript-eslint/parser
+
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  parser: '@babel/eslint-parser',
+  plugins: [
+    'import', 
+    'svelte3',
+    '@typescript-eslint' ,
+    'prettier',
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
-    extraFileExtensions: ['.svelte']
-  },
-  env: {
-    es6: true,
-    browser: true
-  },
-  overrides: [
+  extends: ["eslint:recommended", "plugin:import/recommended"],
+  overrides: [ 
     {
       files: ['*.svelte'],
       processor: 'svelte3/svelte3'
     }
   ],
-  settings: {
-    'svelte3/typescript': require('typescript'),
-    'svelte3/ignore-styles': () => true
+  rules: {
   },
-  plugins: ['svelte3', '@typescript-eslint'],
-  ignorePatterns: ['node_modules']
-}
+  settings: {
+    'svelte3/typescript': true,
+  },
+  env: {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  parserOptions: { 
+    sourceType: "module",
+    ecmaVersion: 2019,
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.svelte'],
+    requireConfigFile: false
+  },
+};
