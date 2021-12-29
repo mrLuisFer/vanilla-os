f797fa3fafbfa10690c53eebefe227a24eca415c
mrLuisFer <mrluisfeer@gmail.com>
2021-12-28 23:29:04 -0600

    create header folder

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import Header from './components/common/Header.svelte';
-  import Login from './pages/Login.svelte';
-  import Home from './pages/Home.svelte';
+  import Header from "./components/common/header/Header.svelte";
+  import Login from "./pages/Login.svelte";
+  import Home from "./pages/Home.svelte";
 
   let name: string = "";
   let password: string = "";
@@ -12,34 +12,35 @@
   {#if name && name.length > 0}
     <Home />
   {:else}
-    <Login bind:name={name} bind:password={password} />
+    <Login bind:name bind:password />
   {/if}
 </main>
 
 <style lang="scss" global>
   :root {
-    font-family: Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans",
+      "Helvetica Neue", sans-serif;
     font-size: 16px;
     // Nord Palette
     --polar-night-0: #2e3440;
     --polar-night-1: #3b4252;
     --polar-night-2: #434c5e;
-    --polar-night-3: #4C566A;
+    --polar-night-3: #4c566a;
 
-    --snow-storm-0: #D8DEE9;
-    --snow-storm-1: #E5E9F0;
-    --snow-storm-2: #ECEFF4;
+    --snow-storm-0: #d8dee9;
+    --snow-storm-1: #e5e9f0;
+    --snow-storm-2: #eceff4;
 
-    --frost-0: #8FBCBB;
-    --frost-1: #88C0D0;
-    --frost-2: #81A1C1;
-    --frost-3: #5E81AC;
+    --frost-0: #8fbcbb;
+    --frost-1: #88c0d0;
+    --frost-2: #81a1c1;
+    --frost-3: #5e81ac;
 
-    --aurora-red: #BF616A;
-    --aurora-orange: #D08770;
-    --aurora-yellow: #EBCB8B;
-    --aurora-green: #A3BE8C;
-    --aurora-purple: #B48EAD;
+    --aurora-red: #bf616a;
+    --aurora-orange: #d08770;
+    --aurora-yellow: #ebcb8b;
+    --aurora-green: #a3be8c;
+    --aurora-purple: #b48ead;
   }
 
   html {
@@ -50,12 +51,12 @@
   *:before,
   *:after {
     box-sizing: inherit;
-		margin: 0;
+    margin: 0;
   }
 
   body {
     margin: 0;
     padding: 0;
-    font-family: 'Roboto', sans-serif;
+    font-family: "Roboto", sans-serif;
   }
 </style>
---
+++ src/components/common/header/Header.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+</script>
+
+<header class="header">
+  <div class="header_tiled-window">
+    <p>Tiled, Float</p>
+  </div>
+  <div class="header_applist">
+    <ul class="header_applist-ul">
+      <li>
+        <i class="fas fa-code"></i>
+      </li>
+      <li>
+        <i class="fab fa-firefox-browser"></i>
+      </li>
+      <li>
+        <i class="fas fa-folder-open"></i>
+      </li>
+      <li>
+        <i class="fas fa-code"></i>
+      </li>
+      <li>
+        <i class="fas fa-book"></i>
+      </li>
+      <li>
+        <i class="fas fa-comments"></i>
+      </li>
+      <li>
+        <i class="fab fa-youtube"></i>
+      </li>
+      <li>
+        <i class="fas fa-cog"></i>
+      </li>
+    </ul>
+  </div>
+</header>
+
+<style lang="scss">
+  .header {
+    display: flex;
+    background: var(--polar-night-0);
+    padding: 0.5rem 1.5rem;
+    align-items: center;
+
+    &_tiled-window {
+      background: var(--aurora-yellow);
+      padding: 0.5rem 0.6rem;
+      font-family: "Roboto Mono", sans-serif;
+
+      p {
+        margin: 0;
+        font-size: 0.8rem;
+      }
+    }
+
+    &_applist {
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      margin-left: 1rem;
+
+      &-ul {
+        list-style: none;
+        padding: 0;
+      }
+    }
+  }
+</style>
